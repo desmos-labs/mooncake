6af62c15b812adef614b1b7a379f334341b60719
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-01-24 15:35:31 +0100

    Run flutter format

--- lib/ui/widgets/posts/list/posts_list.dart
+++ lib/ui/widgets/posts/list/posts_list.dart
@@ -1,6 +1,5 @@
 import 'package:mooncake/entities/entities.dart';
 import 'package:mooncake/ui/ui.dart';
-import 'package:mooncake/ui/widgets/posts/list/fetching_snackbar.dart';
 import 'package:mooncake/ui/widgets/posts/list/sync_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
