225219dc15db9f154cf8815863acb3237465ff78
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-03-06 15:19:24 +0100

    Improved the user structure

--- lib/entities/user/user.dart
+++ lib/entities/user/user.dart
@@ -12,20 +12,11 @@ class User extends Equatable {
   @JsonKey(name: "username", nullable: true)
   final String username;
 
-  /// Returns `true` iff the username is not null and not empty.
-  bool get hasUsername => username != null && username.isNotEmpty;
-
   @JsonKey(name: "avatar_url", nullable: true)
   final String avatarUrl;
 
-  /// Returns `true` iff the user has an associated avatar.
-  bool get hasAvatar => avatarUrl != null && avatarUrl.isNotEmpty;
-
   @JsonKey(name: "account_data")
   final AccountData accountData;
-
-  /// Returns `true` iff the account needs to be funded.
-  bool get needsFunding => accountData.coins.isEmpty;
 
   User({
     this.username,
@@ -33,16 +24,29 @@ class User extends Equatable {
     @required this.accountData,
   }) : assert(accountData != null);
 
-  factory User.fromAddress(String address) => User(
-        accountData: AccountData(
-          address: address,
-          coins: [],
-          accountNumber: 0,
-          sequence: 0,
-        ),
-      );
+  factory User.fromAddress(String address) {
+    return User(
+      accountData: AccountData(
+        address: address,
+        coins: [],
+        accountNumber: 0,
+        sequence: 0,
+      ),
+    );
+  }
 
-  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  factory User.fromJson(Map<String, dynamic> json) {
+    return _$UserFromJson(json);
+  }
+
+  /// Returns `true` iff the user has an associated avatar.
+  bool get hasAvatar => avatarUrl != null && avatarUrl.isNotEmpty;
+
+  /// Returns `true` iff the username is not null and not empty.
+  bool get hasUsername => username != null && username.isNotEmpty;
+
+  /// Returns `true` iff the account needs to be funded.
+  bool get needsFunding => accountData.coins.isEmpty;
 
   Map<String, dynamic> toJson() => _$UserToJson(this);
 
