3f1db78bcd6b3caf4c4e9123eb3993ba42002905
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-05-01 14:09:06 +0200

    Fixed the remote user source test

--- test/sources/user/remote/remote_user_source_impl_test.dart
+++ test/sources/user/remote/remote_user_source_impl_test.dart
@@ -32,60 +32,45 @@ void main() {
     );
   });
 
-  group('getAccountData', () {
-    test('returns null when response is wrong', () async {
-      final response = LcdResponse(height: "1", result: {});
-      when(chainHelper.queryChain(any))
-          .thenAnswer((_) => Future.value(response));
+  test('getAccountData returns null when response is wrong', () async {
+    final response = LcdResponse(height: "1", result: {});
+    server.enqueue(body: jsonEncode(response.toJson()));
 
-      expect(await source.getAccount(""), isNull);
-    });
-
-    test('returns valid data with correct response', () async {
-      final file = File("test_resources/account/account_response.json");
-      final json = jsonDecode(file.readAsStringSync());
-      final accountResponse = LcdResponse.fromJson(json);
-      when(chainHelper.queryChain(any))
-          .thenAnswer((_) => Future.value(accountResponse));
-
-      final account = await source
-          .getAccount("desmos16f9wz7yg44pjfhxyn22kycs0qjy778ng877usl");
-      expect(account, isNotNull);
-      expect(account.cosmosAccount.address,
-          "desmos16f9wz7yg44pjfhxyn22kycs0qjy778ng877usl");
-      expect(account.cosmosAccount.sequence, 39);
-      expect(account.cosmosAccount.accountNumber, 54);
-      expect(account.cosmosAccount.coins,
-          [StdCoin(amount: "10000000", denom: "udaric")]);
-    });
+    expect(await source.getAccount(""), isNull);
   });
 
-  group('fundAccount', () {
-    final account = MooncakeAccount(
-        avatarUrl: null,
-        username: null,
-        cosmosAccount: CosmosAccount(
-          address: "address",
-          sequence: 0,
-          accountNumber: 0,
-          coins: [],
-        ));
+  test('getAccountData returns valid data with correct response', () async {
+    final file = File("test_resources/account/account_response.json");
+    final json = jsonDecode(file.readAsStringSync());
+    final accountResponse = LcdResponse.fromJson(json);
+    server.enqueue(body: jsonEncode(accountResponse));
 
-    test('completes successfully if no exception is thrown', () async {
-      server.dispatcher = (HttpRequest request) async {
-        expect(request.method, "POST");
-        expect(
-          request.headers.value("Content-Type"),
-          contains("application/json"),
-        );
-        expect(request.contentLength, greaterThan(0));
+    final account = await source
+        .getAccount("desmos16f9wz7yg44pjfhxyn22kycs0qjy778ng877usl");
+    expect(account, isNotNull);
+    expect(account.cosmosAccount.address,
+        "desmos16f9wz7yg44pjfhxyn22kycs0qjy778ng877usl");
+    expect(account.cosmosAccount.sequence, 39);
+    expect(account.cosmosAccount.accountNumber, 54);
+    expect(account.cosmosAccount.coins,
+        [StdCoin(amount: "10000000", denom: "udaric")]);
+  });
 
-        return MockResponse()
-          ..httpCode = 200
-          ..body = "Ok";
-      };
+  test('fundAccount completes successfully when no exception', () async {
+    final account = MooncakeAccount.local("address");
+    server.dispatcher = (HttpRequest request) async {
+      expect(request.method, "POST");
+      expect(
+        request.headers.value("Content-Type"),
+        contains("application/json"),
+      );
+      expect(request.contentLength, greaterThan(0));
 
-      expect(source.fundAccount(account), completes);
-    });
+      return MockResponse()
+        ..httpCode = 200
+        ..body = "Ok";
+    };
+
+    expect(source.fundAccount(account), completes);
   });
 }
