7e8121fdff6fa104dad5e2fd8c9ec56cb9a1d956
Wingman <ryuash123@gmail.com>
2020-07-08 15:17:03 +0800

    set up home bloc

--- lib/ui/blocs/home/home_bloc.dart
+++ lib/ui/blocs/home/home_bloc.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:mooncake/dependency_injection/dependency_injection.dart';
 import 'package:mooncake/usecases/usecases.dart';
+import 'package:mooncake/entities/entities.dart';
 
 import '../export.dart';
 
@@ -13,19 +14,38 @@ import '../export.dart';
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final AccountBloc _loginBloc;
   final LogoutUseCase _logoutUseCase;
+  final SaveSettingUseCase _saveSettingUseCase;
+  final WatchSettingUseCase _watchSettingUseCase;
+  final GetSettingUseCase _getSettingUseCase;
+
+  StreamSubscription<dynamic> _watchSettingSubscription;
 
   HomeBloc({
     @required AccountBloc loginBloc,
     @required LogoutUseCase logoutUseCase,
+    @required SaveSettingUseCase saveSettingUseCase,
+    @required WatchSettingUseCase watchSettingUseCase,
+    @required GetSettingUseCase getSettingUseCase,
   })  : assert(loginBloc != null),
         _loginBloc = loginBloc,
         assert(logoutUseCase != null),
-        _logoutUseCase = logoutUseCase;
+        _logoutUseCase = logoutUseCase,
+        assert(saveSettingUseCase != null),
+        _saveSettingUseCase = saveSettingUseCase,
+        assert(watchSettingUseCase != null),
+        _watchSettingUseCase = watchSettingUseCase,
+        assert(getSettingUseCase != null),
+        _getSettingUseCase = getSettingUseCase {
+    _startSubscription();
+  }
 
   factory HomeBloc.create(BuildContext context) {
     return HomeBloc(
       loginBloc: BlocProvider.of(context),
       logoutUseCase: Injector.get(),
+      saveSettingUseCase: Injector.get(),
+      watchSettingUseCase: Injector.get(),
+      getSettingUseCase: Injector.get(),
     );
   }
 
@@ -39,8 +59,53 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     if (event is UpdateTab) {
       yield state.copyWith(activeTab: event.tab);
     } else if (event is SignOut) {
-      await _logoutUseCase.logout();
+      _mapSignOutToState();
       _loginBloc.add(LogOut());
+    } else if (event is ShowBackupMnemonicPhrasePopup) {
+      yield state.copyWith(showBackupPhrasePopup: true);
+    } else if (event is HideBackupMnemonicPhrasePopup) {
+      yield state.copyWith(showBackupPhrasePopup: false);
+    } else if (event is TurnOffBackupMnemonicPopupPermission) {
+      _mapTurnOffBackupMnemonicPopupPermissionToState();
     }
+  }
+
+  void _startSubscription() async {
+    if (_watchSettingSubscription == null) {
+      _watchSettingUseCase
+          .watch(key: SettingKeys.TX_AMOUNT)
+          .listen((value) async {
+        final checkTxAmount = (value == 5) || (value != 0 && value % 10 == 0);
+        final checkPopupPermission = await _getSettingUseCase.get(
+                key: SettingKeys.BACKUP_POPUP_PERMISSION) ??
+            true;
+        if (checkTxAmount && checkPopupPermission == true) {
+          this.add(ShowBackupMnemonicPhrasePopup());
+        } else {
+          this.add(HideBackupMnemonicPhrasePopup());
+        }
+      });
+    }
+
+    // wingman testing
+    await _saveSettingUseCase.save(key: SettingKeys.TX_AMOUNT, value: 5);
+  }
+
+  /// handles SignOut [event]
+  void _mapSignOutToState() async {
+    this._stopSubscription();
+    await _logoutUseCase.logout();
+  }
+
+  /// Stops all valid subscriptons
+  void _stopSubscription() {
+    _watchSettingSubscription?.cancel();
+    _watchSettingSubscription = null;
+  }
+
+  /// handles TurnOffBackupMnemonicPopupPermission [event]
+  _mapTurnOffBackupMnemonicPopupPermissionToState() async {
+    await _saveSettingUseCase.save(
+        key: SettingKeys.BACKUP_POPUP_PERMISSION, value: false);
   }
 }
