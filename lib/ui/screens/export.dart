e01e6d01e34de4cd1b6393f9a725943362ad9529
Wingman <ryuash123@gmail.com>
2020-07-15 15:41:44 +0800

    moved login screens in to separate folders

--- lib/ui/screens/export.dart
+++ lib/ui/screens/export.dart
@@ -1,8 +1,12 @@
-export 'login/export.dart';
+// export 'login/export.dart';
 export 'security/export.dart';
 export 'user/export.dart';
 export 'export_mnemonic_screen.dart';
 export 'post_create_screen.dart';
 export 'home_screen.dart';
 export 'post_details_screen.dart';
-export 'splash_screen.dart';
+// wingman new exports
+export 'splash_screen/export.dart';
+export 'login_screen/export.dart';
+export 'recover_account_screen/export.dart';
+export 'restore_mnemonic_backup_screen/export.dart';
