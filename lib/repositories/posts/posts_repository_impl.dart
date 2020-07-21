ee9535d41d222ec6d8425646e2603fc8f73ee86f
Wingman <ryuash123@gmail.com>
2020-07-21 17:05:46 +0800

    wip: testing

--- lib/repositories/posts/posts_repository_impl.dart
+++ lib/repositories/posts/posts_repository_impl.dart
@@ -59,9 +59,10 @@ class PostsRepositoryImpl extends PostsRepository {
   Future<Post> getPostById(String postId, {bool refresh = false}) async {
     if (refresh) {
       final updated = await _remotePostsSource.getPostById(postId);
-      await _localPostsSource.savePost(updated);
+      print('===i expect null===');
+      print(updated);
+      if (updated == null) await _localPostsSource.savePost(updated);
     }
-
     return _localPostsSource.getPostById(postId);
   }
 
