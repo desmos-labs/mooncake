6e258b92747ead4cfa1fe550eff29f4c78737601
Wingman <ryuash123@gmail.com>
2020-07-21 10:43:10 +0800

    wip: test remove row

--- lib/ui/widgets/post_content/widgets/post_poll_content/widgets/post_poll_result_item/index.dart
+++ lib/ui/widgets/post_content/widgets/post_poll_content/widgets/post_poll_result_item/index.dart
@@ -24,19 +24,43 @@ class PostPollResultItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // print('media thing');
+    // print(MediaQuery.of(context).size.width * percentage);
+    // print('no percentage');
+    // print(MediaQuery.of(context).size.width);
+    print('=================');
+    print(percentage);
     return Container(
       child: Stack(
         alignment: Alignment.centerLeft,
         children: [
-          Positioned.fill(
-            child: Container(
-              width: MediaQuery.of(context).size.width * percentage,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Theme.of(context).accentColor.withOpacity(0.25),
-              ),
+          Container(
+            // width: MediaQuery.of(context).size.width * 1,
+            width: double.infinity,
+            height: 30.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(width: 1.0, color: Colors.blue),
+              // color: Theme.of(context).accentColor.withOpacity(0.25),
             ),
           ),
+          // Positioned.fill(
+          // child:
+          Container(
+            // width: MediaQuery.of(context).size.width * percentage,
+            width: MediaQuery.of(context).size.width * 0.88,
+            height: 30.0,
+            decoration: BoxDecoration(
+              // borderRadius: BorderRadius.only(
+              //   topLeft: Radius.circular(8),
+              //   bottomLeft: Radius.circular(8),
+              // ),
+              borderRadius: BorderRadius.circular(8),
+              // color: Theme.of(context).accentColor.withOpacity(0.25),
+              color: Colors.blue,
+            ),
+          ),
+          // ),
           Padding(
             padding: EdgeInsets.symmetric(
               horizontal: ThemeSpaces.smallMargin,
