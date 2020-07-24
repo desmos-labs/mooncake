09b74c9f04dbd9e0f44dd69d4818c532f1a16925
Wingman J. Lee <42913823+ryuash@users.noreply.github.com>
2020-07-24 12:04:25 +0800

    Updated the post layout
    See PR #92

--- lib/ui/widgets/posts_list_item/index.dart
+++ lib/ui/widgets/posts_list_item/index.dart
@@ -29,7 +29,7 @@ class PostListItem extends StatelessWidget {
     }
 
     return Container(
-      margin: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 0),
+      margin: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
       child: Material(
         color: color,
         borderRadius: BorderRadius.circular(4.0),
@@ -42,7 +42,7 @@ class PostListItem extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 PostContent(post: post),
-                const SizedBox(height: ThemeSpaces.smallMargin),
+                const SizedBox(height: ThemeSpaces.mediumMargin),
                 PostActionsBar(post: post),
                 if (post.reactions.isNotEmpty)
                   const SizedBox(height: ThemeSpaces.smallMargin),
