c99d26a2ca15554272dd60d1f49becedf4aa04ba
Wingman <ryuash123@gmail.com>
2020-07-30 11:30:11 +0800

    fixed: flutter analyze

--- test/test_utils/network_image.dart
+++ test/test_utils/network_image.dart
@@ -41,7 +41,8 @@ import 'package:mockito/mockito.dart';
 ///
 /// The underlying code is taken from the Flutter repo:
 /// https://github.com/flutter/flutter/blob/master/dev/manual_tests/test/mock_image_http.dart
-R provideMockedNetworkImages<R>(R body(), {List<int> imageBytes = _transparentImage}) {
+R provideMockedNetworkImages<R>(R body(),
+    {List<int> imageBytes = _transparentImage}) {
   return HttpOverrides.runZoned(
     body,
     createHttpClient: (_) => _createMockImageHttpClient(_, imageBytes),
@@ -49,39 +50,111 @@ R provideMockedNetworkImages<R>(R body(), {List<int> imageBytes = _transparentIm
 }
 
 class MockHttpClient extends Mock implements HttpClient {}
+
 class MockHttpClientRequest extends Mock implements HttpClientRequest {}
+
 class MockHttpClientResponse extends Mock implements HttpClientResponse {}
+
 class MockHttpHeaders extends Mock implements HttpHeaders {}
 
 // Returns a mock HTTP client that responds with an image to all requests.
-MockHttpClient _createMockImageHttpClient(SecurityContext _, List<int> imageBytes) {
+MockHttpClient _createMockImageHttpClient(
+    SecurityContext _, List<int> imageBytes) {
   final MockHttpClient client = MockHttpClient();
   final MockHttpClientRequest request = MockHttpClientRequest();
   final MockHttpClientResponse response = MockHttpClientResponse();
   final MockHttpHeaders headers = MockHttpHeaders();
 
-  when(client.getUrl(any)).thenAnswer((_) => Future<HttpClientRequest>.value(request));
+  when(client.getUrl(any))
+      .thenAnswer((_) => Future<HttpClientRequest>.value(request));
   when(request.headers).thenReturn(headers);
-  when(request.close()).thenAnswer((_) => Future<HttpClientResponse>.value(response));
+  when(request.close())
+      .thenAnswer((_) => Future<HttpClientResponse>.value(response));
   when(response.contentLength).thenReturn(_transparentImage.length);
   when(response.statusCode).thenReturn(HttpStatus.ok);
   when(response.listen(any)).thenAnswer((Invocation invocation) {
-    final void Function(List<int>) onData = invocation.positionalArguments[0] as void Function(List<int> p1);
-    final void Function() onDone = invocation.namedArguments[#onDone] as void Function();
-    final void Function(Object, [StackTrace]) onError = invocation.namedArguments[#onError] as void Function(Object p1, [StackTrace p2]);
-    final bool cancelOnError = invocation.namedArguments[#cancelOnError] as bool;
+    final void Function(List<int>) onData =
+        invocation.positionalArguments[0] as void Function(List<int> p1);
+    final void Function() onDone =
+        invocation.namedArguments[#onDone] as void Function();
+    final void Function(Object, [StackTrace]) onError = invocation
+        .namedArguments[#onError] as void Function(Object p1, [StackTrace p2]);
+    final bool cancelOnError =
+        invocation.namedArguments[#cancelOnError] as bool;
 
-    return Stream<List<int>>.fromIterable(<List<int>>[imageBytes])
-        .listen(onData, onDone: onDone, onError: onError, cancelOnError: cancelOnError);
+    return Stream<List<int>>.fromIterable(<List<int>>[imageBytes]).listen(
+        onData,
+        onDone: onDone,
+        onError: onError,
+        cancelOnError: cancelOnError);
   });
 
   return client;
 }
 
 const List<int> _transparentImage = <int>[
-  0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49,
-  0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06,
-  0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44,
-  0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01, 0x0D,
-  0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE,
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
 ];
--- test/ui/widgets/bottom_navigation_bar/index_test.dart
+++ test/ui/widgets/bottom_navigation_bar/index_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:mooncake/ui/ui.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mooncake/ui/ui.dart';
 
 void main() {
   testWidgets('Displays correctly', (WidgetTester tester) async {
