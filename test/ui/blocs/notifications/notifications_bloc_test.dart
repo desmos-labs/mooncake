bff9bcd81acacef39dd12a2dcc84f26aeceb92b9
Wingman <ryuash123@gmail.com>
2020-07-31 23:32:40 +0800

    wip: post details bloc test

---
+++ test/ui/blocs/notifications/notifications_bloc_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mooncake/usecases/usecases.dart';
+import 'package:mooncake/ui/ui.dart';
+import 'package:mooncake/entities/entities.dart';
+
+class MockGetNotificationsUseCase extends Mock
+    implements GetNotificationsUseCase {}
+
+void main() {
+  MockGetNotificationsUseCase mockGetNotificationsUseCase;
+
+  setUp(() {
+    mockGetNotificationsUseCase = MockGetNotificationsUseCase();
+  });
+
+  group(
+    'NotificationsBloc',
+    () {
+      NotificationsBloc notificationsBloc;
+      setUp(
+        () {
+          notificationsBloc = NotificationsBloc(
+              getNotificationsUseCase: mockGetNotificationsUseCase);
+        },
+      );
+
+      blocTest(
+        'LoadNotifications: work properly',
+        build: () async {
+          when(mockGetNotificationsUseCase.single())
+              .thenAnswer((_) => Future.value([
+                    TxSuccessfulNotification(
+                        date: DateTime.now(), txHash: "hash-1"),
+                    TxFailedNotification(
+                        date: DateTime.now(), txHash: "hash-2", error: "e"),
+                  ]));
+          return notificationsBloc;
+        },
+        act: (bloc) async {
+          bloc.add(LoadNotifications());
+        },
+        expect: [
+          LoadingNotifications(),
+          NotificationsLoaded([]),
+        ],
+      );
+    },
+  );
+}
