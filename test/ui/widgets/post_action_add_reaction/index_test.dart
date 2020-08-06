bd9533e0c4ddfaa279b13609fac7351ed9b7853e
Wingman <ryuash123@gmail.com>
2020-08-06 12:33:15 +0800

    checkpoint break

--- test/ui/widgets/post_action_add_reaction/index_test.dart
+++ test/ui/widgets/post_action_add_reaction/index_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:emoji_picker/emoji_picker.dart';
 import 'package:mooncake/ui/ui.dart';
 import '../../../mocks/posts.dart';
 import '../helper.dart';
-// import 'package:mooncake/ui/widgets/mnemonic_visualizer/widgets/export.dart';
 
 void main() {
   testWidgets('MnemonicVisualizer: Displays export correctly',
