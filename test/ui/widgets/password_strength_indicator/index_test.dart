5239beeb7537e27bceeff0faa25c52f84dce82a7
Wingman <ryuash123@gmail.com>
2020-08-06 11:50:33 +0800

    add: password strength indicator test

---
+++ test/ui/widgets/password_strength_indicator/index_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:mooncake/ui/ui.dart';
+import '../helper.dart';
+
+void main() {
+  testWidgets('PasswordStrengthIndicator: Displays correctly',
+      (WidgetTester tester) async {
+    PasswordSecurity security = PasswordSecurity.LOW;
+    StateSetter setStateController;
+
+    await tester.pumpWidget(
+      makeTestableWidget(
+        child: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            setStateController = setState;
+
+            return Container(
+              child: PasswordStrengthIndicator(security: security),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+    expect(find.byType(Expanded), findsNWidgets(4));
+    expect(find.text("passwordSecurityLow"), findsOneWidget);
+    expect(
+      tester.widget<Text>(find.byType(Text).last).style.color,
+      Colors.red,
+    );
+
+    setStateController(() {
+      security = PasswordSecurity.MEDIUM;
+    });
+    await tester.pumpAndSettle();
+
+    expect(find.text("passwordSecurityMedium"), findsOneWidget);
+    expect(
+      tester.widget<Text>(find.byType(Text).last).style.color,
+      Colors.orange,
+    );
+
+    setStateController(() {
+      security = PasswordSecurity.HIGH;
+    });
+    await tester.pumpAndSettle();
+
+    expect(find.text("passwordSecurityHigh"), findsOneWidget);
+    expect(
+      tester.widget<Text>(find.byType(Text).last).style.color,
+      Colors.green,
+    );
+  });
+}
