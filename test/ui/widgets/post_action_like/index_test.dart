e28f5a5285c2367bdc0dbf0ba5296ae56b75450f
Wingman <ryuash123@gmail.com>
2020-08-06 12:56:28 +0800

    add: post_action_like test

---
+++ test/ui/widgets/post_action_like/index_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:mooncake/ui/ui.dart';
+import '../../../mocks/posts.dart';
+import '../helper.dart';
+
+void main() {
+  testWidgets('MnemonicVisualizer: Displays export correctly',
+      (WidgetTester tester) async {
+    StateSetter setStateController;
+    bool isLiked = false;
+
+    await tester.pumpWidget(
+      makeTestableWidget(
+        child: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            setStateController = setState;
+            return PostLikeAction(post: testPost, isLiked: isLiked);
+          },
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(MooncakeIcons.heart), findsOneWidget);
+
+    setStateController(() {
+      isLiked = true;
+    });
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(MooncakeIcons.heart), findsNothing);
+    expect(find.byIcon(MooncakeIcons.heartF), findsOneWidget);
+  });
+}
