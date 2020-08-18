430609399bfccf2b6878051971d684e93de6d8d4
Wingman <ryuash123@gmail.com>
2020-08-18 15:16:35 +0800

    update: tests

--- test/ui/screens/login_screen/widgets/account_creating_content/index_test.dart
+++ test/ui/screens/login_screen/widgets/account_creating_content/index_test.dart
@@ -1,21 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mooncake/ui/ui.dart';
 import '../../../../helper.dart';
 import 'package:mooncake/ui/screens/login_screen/widgets/export.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
   testWidgets('CreatingAccountPopupContent: Displays correctly',
       (WidgetTester tester) async {
+    MockAccountBloc mockAccountBloc = MockAccountBloc();
     await tester.pumpWidget(
       makeTestableWidget(
-        child: CreatingAccountPopupContent(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<AccountBloc>(
+              create: (_) => mockAccountBloc,
+            ),
+          ],
+          child: CreatingAccountPopupContent(),
+        ),
       ),
     );
 
     await tester.pump(const Duration(seconds: 3));
 
     expect(find.byType(LoadingIndicator), findsOneWidget);
+    expect(find.byType(SecondaryLightInvertRoundedButton), findsOneWidget);
     expect(find.byType(SizedBox), findsWidgets);
     expect(
       find.text(
@@ -27,5 +38,9 @@ void main() {
       find.text('creatingAccountText'),
       findsOneWidget,
     );
+
+    await tester.tap(find.byType(SecondaryLightInvertRoundedButton));
+    await tester.pump(const Duration(seconds: 3));
+    expect(verify(mockAccountBloc.add(LogOut())).callCount, 1);
   });
 }
