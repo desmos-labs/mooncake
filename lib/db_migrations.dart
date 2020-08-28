dd1be677aa2c7bd54bf2d995f7c3f968bc442867
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-08-28 10:37:10 +0200

    Fixed tests and analyses

--- lib/db_migrations.dart
+++ lib/db_migrations.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mooncake/entities/entities.dart';
 import 'package:sembast/sembast.dart';
