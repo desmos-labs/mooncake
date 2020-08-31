eef6d04378759f71db843dc1ebf2591fd6c13fa3
Wingman <ryuash123@gmail.com>
2020-08-31 11:34:42 +0800

    wip: testing bloc

--- lib/ui/screens/user_details_screen/widgets/menu_drawer/index.dart
+++ lib/ui/screens/user_details_screen/widgets/menu_drawer/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mooncake/entities/entities.dart';
 import 'package:mooncake/ui/ui.dart';
 
@@ -45,9 +46,9 @@ class MenuDrawer extends StatelessWidget {
     return results;
   }
 
-  _handleCreateNewAccount() {
+  void _handleCreateNewAccount(BuildContext context) {
     print("clicked new account");
-    // if account is created
+    BlocProvider.of<AccountBloc>(context).add(GenerateAccountWhileLoggedIn());
   }
 
   _handleImportMnemonic() {
@@ -56,65 +57,74 @@ class MenuDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Container(
-        color: Colors.white,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            SafeArea(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-                child: Row(
-                  children: [
-                    AccountAvatar(user: user, size: 50),
-                    SizedBox(width: 10),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocBuilder<AccountBloc, AccountState>(
+      builder: (context, state) {
+        // wingman
+        print("---here----");
+        print(state);
+        return Drawer(
+          child: Container(
+            color: Colors.white,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                SafeArea(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                    child: Row(
                       children: [
-                        Text(
-                            '${PostsLocalizations.of(context).translate(Messages.hello)},',
-                            style:
-                                Theme.of(context).textTheme.headline6.copyWith(
+                        AccountAvatar(user: user, size: 50),
+                        SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                '${PostsLocalizations.of(context).translate(Messages.hello)},',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .copyWith(
                                       fontSize: 16,
                                     )),
-                        Text(user.screenName,
-                            style: Theme.of(context).textTheme.headline6),
+                            Text(user.screenName,
+                                style: Theme.of(context).textTheme.headline6),
+                          ],
+                        )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
-              ),
+                ..._listAccounts(context),
+                SizedBox(height: ThemeSpaces.mediumGutter),
+                GestureDetector(
+                  onTap: _handleImportMnemonic,
+                  child: Text(
+                    PostsLocalizations.of(context)
+                        .translate(Messages.importMnemonic),
+                    style: Theme.of(context).textTheme.subtitle1.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: ThemeSpaces.mediumGutter),
+                GestureDetector(
+                  onTap: () => _handleCreateNewAccount(context),
+                  child: Text(
+                    PostsLocalizations.of(context)
+                        .translate(Messages.createAnotherAccount),
+                    style: Theme.of(context).textTheme.subtitle1.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-            ..._listAccounts(context),
-            SizedBox(height: ThemeSpaces.mediumGutter),
-            GestureDetector(
-              onTap: _handleImportMnemonic,
-              child: Text(
-                PostsLocalizations.of(context)
-                    .translate(Messages.importMnemonic),
-                style: Theme.of(context).textTheme.subtitle1.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: ThemeSpaces.mediumGutter),
-            GestureDetector(
-              onTap: _handleCreateNewAccount,
-              child: Text(
-                PostsLocalizations.of(context)
-                    .translate(Messages.createAnotherAccount),
-                style: Theme.of(context).textTheme.subtitle1.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
