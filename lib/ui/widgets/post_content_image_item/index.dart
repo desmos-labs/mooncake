4b3b01be1980b71c3da1948db1cd747db93c9c0d
Wingman <ryuash123@gmail.com>
2020-09-24 15:08:30 +0800

    pre testing

--- lib/ui/widgets/post_content_image_item/index.dart
+++ lib/ui/widgets/post_content_image_item/index.dart
@@ -4,8 +4,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mooncake/entities/entities.dart';
 import 'package:mooncake/ui/ui.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Represents a single image items that should be displayed as part
 /// of the post content.
